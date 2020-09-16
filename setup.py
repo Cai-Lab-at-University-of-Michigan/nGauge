a2aa0461befc9ca6ad95e1ff34ada0061b1ebec6
Logan Walker <loganaw@umich.edu>
2020-09-16 10:29:19 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Setup script for nMeasure"""
+
+import os.path
+from setuptools import setup
+
+# The text of the README file
+HERE = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="nMeasure",
+    version="0.1",
+    description="Perform morphology measurement on neuron SWC files",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GeekLogan/nMeasure/",
+    author="Logan Walker",
+    author_email="logan.walker@me.com",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["nMeasure"],
+    include_package_data=True,
+    install_requires=[
+        "scipy", "numpy"
+    ],
+    #entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)
