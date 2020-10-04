a3e9b831b2a30ad2c539ab4e3504081425678085
Logan Walker <logan.walker@me.com>
2020-10-03 22:51:36 -0400

    Fix import order

--- ngauge/__init__.py
+++ ngauge/__init__.py
@@ -9,5 +9,8 @@ __author__ = "Logan A Walker <loganaw@umich.edu> and Jennifer Willams <jenwill@u
 __credits__ = "The University of Michigan"
 
 import ngauge.util
-import ngauge.TracingPoint
-import ngauge.Neuron
+__num_types__ = util.__num_types__
+
+from ngauge.util import *
+from ngauge.TracingPoint import TracingPoint
+from ngauge.Neuron import Neuron
