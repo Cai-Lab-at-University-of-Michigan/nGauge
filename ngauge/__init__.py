82e3801838c4207120ca51e3c62fd8cca1e7de2b
Logan Walker <loganaw@umich.edu>
2021-01-08 00:53:10 -0500

    Update Version Information

--- ngauge/__init__.py
+++ ngauge/__init__.py
@@ -4,7 +4,7 @@ nGauge
 A python library for neuron morphology.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = (
     "Logan A Walker <loganaw@umich.edu> and Jennifer Willams <jenwill@umich.edu>"
 )
