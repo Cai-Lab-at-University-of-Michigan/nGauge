e704080f27eca961efb923cbcc143d4a6219e89c
Logan Walker <logan.walker@me.com>
2021-07-28 15:39:46 -0400

    Preincrement version number

--- ngauge/__init__.py
+++ ngauge/__init__.py
@@ -4,7 +4,7 @@ nGauge
 A python library for neuron morphology.
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = (
     "Logan A Walker <loganaw@umich.edu> and Jennifer Willams <jenwill@umich.edu>"
 )
