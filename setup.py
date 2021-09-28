6cfa507f8a5e50022b570fa384e0b005aed68d1e
Logan Walker <logan.walker@me.com>
2021-09-28 11:50:13 -0400

    Fix typo in sphinx config

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ with open(os.path.join(HERE, "README.md")) as fid:
 
 
 ### Get version number
-version = ''
-for line in open('ngauge/__init__.py'):
-    if '__version__' in line:
-        line = line.split( '=' )[1]
-        line = line.replace( '"', '' )
-        line = line.replace( "'", '' )
+version = ""
+for line in open("ngauge/__init__.py"):
+    if "__version__" in line:
+        line = line.split("=")[1]
+        line = line.replace('"', "")
+        line = line.replace("'", "")
         line = line.strip()
 
         version = line
         break
 
 if not version:
-    raise ValueError( "No Version Detected!!!" )
+    raise ValueError("No Version Detected!!!")
 ###
 
 # This call to setup() does all the work
@@ -37,24 +37,20 @@ setup(
     author_email="loganaw@umich.edu",
     license="GPLv3",
     classifiers=[
-	"Development Status :: 3 - Alpha",
-	"Intended Audience :: Science/Research",
-	"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-	"Natural Language :: English",
-
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3 :: Only",
-
-	"Topic :: Scientific/Engineering :: Visualization",
-	"Topic :: Scientific/Engineering :: Image Processing",
-	"Topic :: Scientific/Engineering :: Bio-Informatics",
-	"Topic :: Scientific/Engineering"
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering",
     ],
     packages=["ngauge"],
     include_package_data=True,
-    install_requires=[
-        "scipy", "numpy", "shapely"
-    ],
-    #entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+    install_requires=["scipy", "numpy", "shapely"],
+    # entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
 )
