929afad2fc524fe6dbeacfa54385afaa422db9f9
Logan Walker <logan.walker@me.com>
2021-11-09 00:43:41 -0500

    Adding m2r2 config

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -33,7 +33,10 @@ release = ngauge.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "m2r2"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
