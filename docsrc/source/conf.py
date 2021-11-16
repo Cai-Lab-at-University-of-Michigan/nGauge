18e268cc299144d468245fff07a75bc928f6db40
Logan Walker <logan.walker@me.com>
2021-11-16 12:12:40 -0500

    Adding more restructuring of manual

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import ngauge
 import os
 import sys
 
@@ -24,7 +25,6 @@ author = "Logan Walker & Jennifer Williams"
 
 # The full version, including alpha/beta/rc tags
 
-import ngauge
 
 release = ngauge.__version__
 
@@ -33,10 +33,7 @@ release = ngauge.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "m2r2"
-]
+extensions = ["sphinx.ext.autodoc", "m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -61,3 +58,12 @@ html_static_path = ["_static"]
 latex_elements = {"extraclassoptions": "openany,oneside"}
 
 master_doc = "index"
+
+html_sidebars = {
+    "**": [
+        "globaltoc.html",
+#        "relations.html",
+        "sourcelink.html",
+        "searchbox.html"
+        ]
+}
